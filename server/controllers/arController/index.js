ee867e8606f977e8eca2da097867fc4fa35b49fb
VincentKempers <me@vncn.tk>
2018-05-08 15:50:18 +0200

    add d3 route and ar map in views

--- server/controllers/arController/index.js
+++ server/controllers/arController/index.js
@@ -1,3 +1,7 @@
 exports.homePage = (req, res) => {
 	res.render('homePage', { message: 'Homepage for AR' });
 };
+
+exports.d3Tests = (req, res) => {
+	res.render('d3', { message: 'd3 test page' });
+};
--- server/routes/arRoutes.js
+++ server/routes/arRoutes.js
@@ -4,5 +4,6 @@ const arController = require('../controllers/arController');
 const router = express.Router();
 
 router.get('/', arController.homePage);
+router.get('/d3', arController.d3Tests);
 
 module.exports = router;
