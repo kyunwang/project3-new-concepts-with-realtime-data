4d5a67a66b689fd46d612f4699a6712a0e79af80
Vincent Kempers <VincentKempers@users.noreply.github.com>
2018-05-11 18:42:33 +0200

    Feature/zero state (#15)
    * Feature/ar d3js (#7)
    
    * Added links to the tools readme
    
    * Divided controllers for the website and ar respectable
    
    * Add webite folder in views
    
    * add d3 route and ar map in views
    
    * Add simple ar initialization (#1)
    
    * Feature/website structure (#3)
    
    * Add basic styling
    
    * Add homepage setup
    
    * Add button styles
    
    * Add Open Sans Google font link
    
    * Add basic homepage content
    
    * Test render d3 charts with ar js
    
    * Feature/website ar button (#4)
    
    * Add static AR button styles
    
    * Add href to AR link, empty for now
    
    * Add button hover and focus styles
    
    * Add two custom patterns for testing
    
    * Feature/initialize d3 (#2)
    
    * add dummy data
    
    * add dummy graph for the data
    
    * add route for api test
    
    * add controller for api config
    
    * add the view for the api content
    
    * add @stompjs
    
    * add the url
    
    * Update index.js
    
    Changed the page message
    
    *  Add conditionals to scripts and added vendor scripts
    
    * Change tour route setting to include vendor js
    
    * Feature/initialize d3 (#5)
    
    * add dummy data
    
    * add dummy graph for the data
    
    * add route for api test
    
    * add controller for api config
    
    * add the view for the api content
    
    * add @stompjs
    
    * add the url
    
    * Update index.js
    
    Changed the page message
    
    * fixed everything
    
    * Add H1 styles (#6)
    
    * Aligned the bars with same origin
    
    * initial zerostate commit
    
    Added zeroState page + css
    Updated title in header
    Added zeroState in router
    Added zeroState AR marker
    
    * id fix
    
    changed id to class

--- server/controllers/websiteController/index.js
+++ server/controllers/websiteController/index.js
@@ -5,3 +5,7 @@ exports.homePage = (req, res) => {
 exports.infoPage = (req, res) => {
 	res.render('website/infoPage');
 };
+
+exports.zeroState = (req, res) => {
+	res.render('website/zeroState', { message: 'Zero state' });
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,9 @@ const wController = require('../controllers/websiteController');
 const router = express.Router();
 
 router.get('/', wController.homePage);
+
+router.get('/zeroState', wController.zeroState);
+
 router.get('/home', wController.infoPage);
 
 module.exports = router;
